7f25dd89f50fad3f8f7701da469362bb34599e88
Thai <pumpo6969@gmail.com>
2025-09-05 13:10:40 +0700

    server

--- backend-tutorial/server.js
+++ backend-tutorial/server.js
@@ -1,17 +1,22 @@
-const app = require('./app');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+// server.js
+const app = require("./app");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-dotenv.config();
+dotenv.config(); // Load .env
 
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
-mongoose.connect(MONGO_URI)
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
-    console.log('MongoDB connected');
+    console.log("MongoDB connected");
     app.listen(PORT, () => {
       console.log(`Server running at http://localhost:${PORT}`);
     });
   })
-  .catch((err) => console.error(err));
+  .catch((err) => console.error("MongoDB connection error:", err));
